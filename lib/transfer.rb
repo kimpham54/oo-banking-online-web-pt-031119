8d424099b8e118eb52be5a4d44a068096e5bb59b
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-27 02:42:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+
+attr_accessor :sender, :receiver, :status
+
+  def initialize(sender, receiver)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
 end
