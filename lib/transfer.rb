7a00dab111dbdc6c599acde26d1048b7fecdff03
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-27 03:12:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,6 +32,7 @@ def reverse_transfer
   if @status == "complete"
     @sender.balance = @sender.balance + @amount
     @receiver.balance = @receiver.balance - @amount
+    @status = "reversed"
   end
 end
 
